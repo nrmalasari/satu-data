86d6f308fba923b4f2e7c62809cad8555eb19e70
nirmalasar <nirmalamala1311@gmail.com>
2025-04-30 11:10:49 +0800

    perbaikan vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-  base: process.env.VITE_BASE_PATH || "/satu-data",
+  base: isProduction ? "/satu-data/" : "/", // base kosong saat dev
 });
